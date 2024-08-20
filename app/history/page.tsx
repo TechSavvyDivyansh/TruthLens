35154c23e78ac32a07f54ab982a8809a7dea5682
Aditi <aditisbadkur@gmail.com>
2024-08-21 00:38:54 +0530

    history basics

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { ExpandableCardDemo } from '@/components/ui/expandable-card';
 import { useTheme } from 'next-themes';
 import React from 'react'
 
@@ -6,7 +7,7 @@ export default function history() {
   const { theme, setTheme } = useTheme();
   return (
     <div className={`w-[99%] h-[79%] rounded-xl ml-5 p-5 ${theme === 'dark' ? 'bg-gray-800 text-white' : 'bg-gray-100 text-black'}`}>
-      history
+      <ExpandableCardDemo />
     </div>
   )
 }
